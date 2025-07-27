d05e92d368675b4a7839c07ecf5f4dc3e902637b
Yvan Sraka <yvan@sraka.xyz>
2025-07-27 18:54:04 +0200

    25.05

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs.url = "https://channels.nixos.org/nixos-25.05/nixexprs.tar.xz";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    nixpkgs.outputs
+    // flake-utils.lib.eachDefaultSystem (system: {
+      packages.default = nixpkgs.legacyPackages.${system}.writeShellScriptBin "install" ''
+        HARDWARE_LIST=${./hardware-list.tsv} ${./scripts/install}
+      '';
+    })
+    // {
+      # If you're using flakes and still want the opinionated defaults for your
+      # desktop or laptop computers, do something like:
+      #
+      #   my_computer = ricochets.lib.nixosSystem {
+      #     system = "x86_64-linux";
+      #     modules = [
+      #       ./path/to/your/configuration.nix
+      #       ricochets.nixosModules.default
+      #     ];
+      #   };
+      nixosModules.default = ./nixos/opiniated.nix;
+    };
+}
---
+++ nixos/default.nix
@@ -0,0 +1,12 @@
+# NixOS system builder (used by channels)
+# This is invoked when users run nixos-rebuild with ricochets channel
+{
+  configuration ? ./configuration.nix, # Default to ricochets config
+  system ? builtins.currentSystem, # Auto-detect system architecture
+  ... # FIXME: do not cancel out other @inputs...
+}:
+let
+  sources = import ../npins; # Load pinned nixpkgs version
+  nixos = import "${sources.nixpkgs}/nixos"; # NixOS module system
+in
+nixos { inherit configuration system; }
